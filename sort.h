d9f92acf59e69a7db45c7293ca3b288692cb97d0
Charawey-X <charawey@gmail.com>
2022-07-20 21:55:56 +0300

    big o & sorting algorithms

--- sort.h
+++ sort.h
@@ -1,9 +1,8 @@
-#ifndef _SORTING_H_
-#define _SORTING_H_
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
 #include <stdio.h>
-
-
-/** Struct Double Linked List */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,17 +18,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** Given FUN prototype */
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
